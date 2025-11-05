0d455c118977327ad51c89ed0a729de9f2aa8df8
Songtaeha <songtaeha@naver.com>
2025-11-05 15:49:03 +0900

    vite + ts 환경 설정

---
+++ PawTuneWeb/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1 className="title">🐾 PawTune</h1>
+      <button className="start-btn">시작하기</button>
+    </div>
+  );
+}
